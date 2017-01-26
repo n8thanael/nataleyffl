58fdc33ede406041ecf65e81ce50aa54f30dc23c
n8thanael <n8thanael@gmail.com>
2017-01-26 15:25:43 -0500

    Demo

--- web/index.php
+++ web/index.php
@@ -24,56 +24,62 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-
-$app->run();
-
-
-
+$app->get('/api/' function() use ($app) {
+    $app['monolog']->addDebug('logging output.');
 echo 'greetings planetoit<br>' . _isCurl();;
 
 function _isCurl(){
     return function_exists('curl_version');
 }
 
-Bigcommerce::configure(array(
-    'store_url' => 'https://store-yiaomofyl1.mybigcommerce.com',
-    'username'  => 'n8thanael',
-    'api_key'   => '27829dd700728b0e723d457bb1eca1d25e11bac4'
-));
+  Bigcommerce::configure(array(
+      'store_url' => 'https://store-yiaomofyl1.mybigcommerce.com',
+      'username'  => 'n8thanael',
+      'api_key'   => '27829dd700728b0e723d457bb1eca1d25e11bac4'
+  ));
 
-/*
-Bigcommerce::configure(array(
-    'client_id' => '991f5nw2mjoukseygibqn5idsl9haqd',
-    'auth_token' => '63x7yadlpx20svh9543xaeqhtjc0j7s',
-    'store_hash' => 'yiaomofyl1'
-));
-*/
+  /*
+  Bigcommerce::configure(array(
+      'client_id' => '991f5nw2mjoukseygibqn5idsl9haqd',
+      'auth_token' => '63x7yadlpx20svh9543xaeqhtjc0j7s',
+      'store_hash' => 'yiaomofyl1'
+  ));
+  */
 
-Bigcommerce::verifyPeer(false);
+  Bigcommerce::verifyPeer(false);
 
-$ping = Bigcommerce::getTime();
+  $ping = Bigcommerce::getTime();
 
-if(!$ping){
-	$error = Bigcommerce::getLastError();
-	echo '<pre>';
-	print_r($error);
-	echo '</pre>';
-} else {
-	echo $ping->format('H:i:s');	
+  if(!$ping){
+    $error = Bigcommerce::getLastError();
+    echo '<pre>';
+    print_r($error);
+    echo '</pre>';
+  } else {
+    echo $ping->format('H:i:s');  
+  }
+
+  $products = Bigcommerce::getProducts();
+
+  foreach ($products as $product){
+    echo $product->id . " - ";  
+    echo $product->name . " $";
+    echo $product->price . "<br>";
+  }
+
+  $product = Bigcommerce::getProduct(80);
+
+  echo $product->name;
+  echo $product->price;
+
+  $count = Bigcommerce::getProductsCount();
+  echo "<p>" . $count . "</p>";
+
 }
 
-$products = Bigcommerce::getProducts();
 
-foreach ($products as $product){
-	echo $product->id . " - ";	
-	echo $product->name . " $";
-	echo $product->price . "<br>";
-}
+$app->run();
 
-$product = Bigcommerce::getProduct(80);
 
-echo $product->name;
-echo $product->price;
 
-$count = Bigcommerce::getProductsCount();
-echo "<p>" . $count . "</p>";
+
